fd5974cad31e310b17e52fe3df9c15406305a50f
Teo Niemirepo <teo825@gmail.com>
2020-11-22 22:47:35 +0200

    more debug

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ use embedded_graphics::fonts::{Font6x8, Text};
 use embedded_graphics::primitives::Rectangle;
 use embedded_graphics::{primitive_style, text_style};
 use gd32vf103xx_hal::pac;
-// use gd32vf103xx_hal::pac::{I2C0};
-use gd32vf103xx_hal::pac::{I2C0};
-// use crate::pac::{I2C0};
+use gd32vf103xx_hal::pac::I2C0;
+// use gd32vf103xx_hal::spi::{Spi, MODE_0};
+use gd32vf103xx_hal::i2c::{BlockingI2c, Mode};
 use gd32vf103xx_hal::prelude::*;
 
 use gd32vf103xx_hal::gpio::gpiob::{ PB6, PB7 };
@@ -49,6 +49,8 @@ fn main() -> ! {
     let gpioa = periph.GPIOA.split(&mut rcu);
     let gpiob = periph.GPIOB.split(&mut rcu);
 
+    let i2c_0 = periph.I2C0;
+
 
     let lcd_pins = lcd_pins!(gpioa, gpiob);
     let mut lcd = lcd::configure(periph.SPI0, lcd_pins, &mut afio, &mut rcu);
@@ -67,30 +69,27 @@ fn main() -> ! {
     );
 
     let mut delay = McycleDelay::new(&rcu.clocks);
-    delay.delay_ms(100);
+    delay.delay_ms(2);
 
-    let i2c_0 = periph.I2C0;
 
     let scl = gpiob.pb6.into_alternate_open_drain();
     let sda = gpiob.pb7.into_alternate_open_drain();
     
-    let mut i2c_handle = hal::i2c::BlockingI2c::i2c0 (
+    let mut i2c_handle = BlockingI2c::i2c0 (
         i2c_0,
         (scl, sda),
         &mut afio,
-        hal::i2c::Mode::standard(100.khz()),
+        Mode::standard(99500.hz()),
         &mut rcu,
-        100000,
+        50,
         1,
-        100000,
-        100000
+        10000,
+        200
     );
 
     // i2c_handle.write(0x52, &[0xF0, 0x55, 0xFB, 0x00]).unwrap();
     
     let mut write_res = i2c_handle.write(0x52, &[0x40, 0x00]);
-
-    // TODO: somehow sends the address as 8bit instead of 7bit
 
 
     let mut i: u32 = 0;
@@ -120,9 +119,11 @@ fn main() -> ! {
             .draw(&mut lcd).unwrap();
 
 
-        let mut read_buf: [u8; 6] = [0; 6];
+        let mut read_buf: [u8; 5] = [0; 5];
         let read_res = i2c_handle.read(0x52, &mut read_buf);
         f_val = read_buf[0];
+
+        delay.delay_us(100);
 
         write_res = i2c_handle.write(0x52, &[0x00 as u8]);
 
