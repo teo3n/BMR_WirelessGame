c18c674fb41f359457efcef28b8b67a3ab479315
Teo Niemirepo <teo825@gmail.com>
2020-11-24 23:19:04 +0200

    added todo

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,17 @@ use embedded_graphics::fonts::{Font6x8, Text};
 use embedded_graphics::primitives::Rectangle;
 use embedded_graphics::{primitive_style, text_style};
 use gd32vf103xx_hal::pac;
-// use gd32vf103xx_hal::pac::I2C0;
+use gd32vf103xx_hal::pac::I2C1;
 // use gd32vf103xx_hal::spi::{Spi, MODE_0};
-// use gd32vf103xx_hal::i2c::{BlockingI2c, Mode};
+use gd32vf103xx_hal::i2c::{BlockingI2c, Mode};
 use gd32vf103xx_hal::prelude::*;
 
-use gd32vf103xx_hal::gpio::gpiob::{ PB6, PB7 };
+use gd32vf103xx_hal::gpio::gpiob::{ PB8, PB9 };
 
 use gd32vf103xx_hal::rcu::RcuExt;
 use gd32vf103xx_hal::delay::McycleDelay;
 use embedded_hal::blocking::delay::DelayMs;
 
-pub mod lcd;
-pub mod softi2c;
-use softi2c::SoftwareI2c;
-// pub mod led;
 use riscv_rt::entry;
 pub mod nunchuk;
 
@@ -39,89 +35,52 @@ pub mod nunchuk;
 fn main() -> ! {
     let periph = pac::Peripherals::take().unwrap();
 
-    // Configure clocks
     let mut rcu = periph
         .RCU
         .configure()
         .ext_hf_clock(8.mhz())
         .sysclk(108.mhz())
         .freeze();
+
     let mut afio = periph.AFIO.constrain(&mut rcu);
-
-    let gpioa = periph.GPIOA.split(&mut rcu);
     let gpiob = periph.GPIOB.split(&mut rcu);
+    let i2c_0 = periph.I2C0;
+    let mut delay = McycleDelay::new(&rcu.clocks);
+    delay.delay_ms(2);
 
-    // let i2c_0 = periph.I2C0;
-
-
-    let lcd_pins = lcd_pins!(gpioa, gpiob);
-    let mut lcd = lcd::configure(periph.SPI0, lcd_pins, &mut afio, &mut rcu);
-    let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
-
-    // Clear screen
-    Rectangle::new(Point::new(0, 0), Point::new(width - 1, height - 1))
-        .into_styled(primitive_style!(fill_color = Rgb565::BLACK))
-        .draw(&mut lcd)
-        .unwrap();
-
-    let style = text_style!(
-        font = Font6x8,
-        text_color = Rgb565::BLACK,
-        background_color = Rgb565::GREEN
+    let scl = gpiob.pb8.into_alternate_open_drain();
+    let sda = gpiob.pb9.into_alternate_open_drain();
+    
+    let mut i2c_handle = BlockingI2c::i2c0 (
+        i2c_0,
+        (scl, sda),
+        &mut afio,
+        Mode::standard(100.khz()),
+        &mut rcu,
+        998,
+        1,
+        998,
+        998
     );
 
-    let mut delay = McycleDelay::new(&rcu.clocks);
+    // TODO: submit bugreport on incorrect master read ACK
 
-    let scl = gpiob.pb6.into_alternate_open_drain();
-    let sda = gpiob.pb7.into_alternate_open_drain();
-    
-    // let mut i2c_handle = BlockingI2c::i2c0 (
-    //     i2c_0,
-    //     (scl, sda),
-    //     &mut afio,
-    //     Mode::standard(100.khz()),
-    //     &mut rcu,
-    //     1000,
-    //     1,
-    //     10000,
-    //     300
-    // );
+    // i2c_handle.write(0x52, &[0xF0, 0x55, 0xFB, 0x00]);
+    let shit = i2c_handle.write(0x52, &[0x40, 0x00]);
 
-    let mut i2c_handle = SoftwareI2c::new(
+    delay.delay_ms(10);
 
-        );
+    // let shit = i2c_handle.write(0x52, &[0x00]);
 
-    i2c_handle.write(0x52, &[0xF0, 0x55, 0xFB, 0x00]);
-    
-    // let mut write_res = i2c_handle.write(0x52, &[0x40, 0x00]);
-    // i2c_handle.write(0x52, &[0xF0, 0x55]);
-    // write_res = i2c_handle.write(0x52, &[0xFB, 0x00]);
-    // write_res = i2c_handle.write(0x52, &[0x00]);
-
-
-    let mut i: u32 = 0;
-    let mut read_buf: [u8; 6] = [0; 6];
 
     loop
     {
-        let mut print_buf = ArrayString::<[_; 24]>::new();
+        let mut read_buf: [u8; 2] = [0; 2];
+        let read_shit = i2c_handle.read(0x52, &mut read_buf);
 
-        // write!(&mut print_buf, "{}: iw: {}, r: {}, w: {}", i, init_write, read_ok, write_ok).expect("failed to create buffer");
-        write!(&mut print_buf, "{}: {} {} {} {}", i, read_buf[0], read_buf[1], read_buf[2], read_buf[3]).expect("failed to create buffer");
+        delay.delay_us(100);
+        let write_white = i2c_handle.write(0x52, &[0x00]);
 
-
-        Text::new(&print_buf, Point::new(10, 35))
-            .into_styled(style)
-            .draw(&mut lcd).unwrap();
-
-        i2c_handle.read(0x52, &mut read_buf);
-
-        // delay.delay_us(200);
-
-        i2c_handle.write(0x52, &[0x00]);
-
-
-        i += 1;
-        delay.delay_ms(10);
+        delay.delay_ms(100);
     }
 }
