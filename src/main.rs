a91e76481fdc08b5f23023a66632be3bb40fbf86
Teo Niemirepo <teo825@gmail.com>
2020-11-24 19:27:33 +0200

    began messing around with software i2c

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ use embedded_graphics::fonts::{Font6x8, Text};
 use embedded_graphics::primitives::Rectangle;
 use embedded_graphics::{primitive_style, text_style};
 use gd32vf103xx_hal::pac;
-use gd32vf103xx_hal::pac::I2C0;
+// use gd32vf103xx_hal::pac::I2C0;
 // use gd32vf103xx_hal::spi::{Spi, MODE_0};
-use gd32vf103xx_hal::i2c::{BlockingI2c, Mode};
+// use gd32vf103xx_hal::i2c::{BlockingI2c, Mode};
 use gd32vf103xx_hal::prelude::*;
 
 use gd32vf103xx_hal::gpio::gpiob::{ PB6, PB7 };
@@ -29,6 +29,8 @@ use gd32vf103xx_hal::delay::McycleDelay;
 use embedded_hal::blocking::delay::DelayMs;
 
 pub mod lcd;
+pub mod softi2c;
+use softi2c::SoftwareI2c;
 // pub mod led;
 use riscv_rt::entry;
 pub mod nunchuk;
@@ -49,7 +51,7 @@ fn main() -> ! {
     let gpioa = periph.GPIOA.split(&mut rcu);
     let gpiob = periph.GPIOB.split(&mut rcu);
 
-    let i2c_0 = periph.I2C0;
+    // let i2c_0 = periph.I2C0;
 
 
     let lcd_pins = lcd_pins!(gpioa, gpiob);
@@ -69,78 +71,57 @@ fn main() -> ! {
     );
 
     let mut delay = McycleDelay::new(&rcu.clocks);
-    delay.delay_ms(2);
-
 
     let scl = gpiob.pb6.into_alternate_open_drain();
     let sda = gpiob.pb7.into_alternate_open_drain();
     
-    let mut i2c_handle = BlockingI2c::i2c0 (
-        i2c_0,
-        (scl, sda),
-        &mut afio,
-        Mode::standard(99500.hz()),
-        &mut rcu,
-        50,
-        1,
-        10000,
-        200
-    );
+    // let mut i2c_handle = BlockingI2c::i2c0 (
+    //     i2c_0,
+    //     (scl, sda),
+    //     &mut afio,
+    //     Mode::standard(100.khz()),
+    //     &mut rcu,
+    //     1000,
+    //     1,
+    //     10000,
+    //     300
+    // );
 
-    // i2c_handle.write(0x52, &[0xF0, 0x55, 0xFB, 0x00]).unwrap();
+    let mut i2c_handle = SoftwareI2c::new(
+
+        );
+
+    i2c_handle.write(0x52, &[0xF0, 0x55, 0xFB, 0x00]);
     
-    let mut write_res = i2c_handle.write(0x52, &[0x40, 0x00]);
+    // let mut write_res = i2c_handle.write(0x52, &[0x40, 0x00]);
+    // i2c_handle.write(0x52, &[0xF0, 0x55]);
+    // write_res = i2c_handle.write(0x52, &[0xFB, 0x00]);
+    // write_res = i2c_handle.write(0x52, &[0x00]);
 
 
     let mut i: u32 = 0;
-    let mut f_val: u8 = 0;
-
-    let mut read_ok = 0;
-    let mut write_ok = 0;
-    let mut init_write = 0;
-
-    match write_res
-    {
-        Ok(x) => init_write = 1,
-        _ => init_write = 0,
-    }
-
+    let mut read_buf: [u8; 6] = [0; 6];
 
     loop
     {
         let mut print_buf = ArrayString::<[_; 24]>::new();
 
-        write!(&mut print_buf, "{}: iw: {}, r: {}, w: {}", i, init_write, read_ok, write_ok).expect("failed to create buffer");
-        // write!(&mut print_buf, "{}: {} {} {} {}", i, read_buf[0], read_buf[1], read_buf[2], read_buf[3]).expect("failed to create buffer");
+        // write!(&mut print_buf, "{}: iw: {}, r: {}, w: {}", i, init_write, read_ok, write_ok).expect("failed to create buffer");
+        write!(&mut print_buf, "{}: {} {} {} {}", i, read_buf[0], read_buf[1], read_buf[2], read_buf[3]).expect("failed to create buffer");
 
 
         Text::new(&print_buf, Point::new(10, 35))
             .into_styled(style)
             .draw(&mut lcd).unwrap();
 
+        i2c_handle.read(0x52, &mut read_buf);
 
-        let mut read_buf: [u8; 5] = [0; 5];
-        let read_res = i2c_handle.read(0x52, &mut read_buf);
-        f_val = read_buf[0];
+        // delay.delay_us(200);
 
-        delay.delay_us(100);
+        i2c_handle.write(0x52, &[0x00]);
 
-        write_res = i2c_handle.write(0x52, &[0x00 as u8]);
-
-
-        match read_res
-        {
-            Ok(x) => read_ok = 1,
-            _ => read_ok = 0,
-        }
-        
-        match write_res
-        {
-            Ok(x) => write_ok = 1,
-            _ => write_ok = 0,
-        }
 
         i += 1;
-        delay.delay_ms(100);
+        delay.delay_ms(10);
     }
 }
